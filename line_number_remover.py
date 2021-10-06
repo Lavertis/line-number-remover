e93071493ccaa9d8eebe093e493a9a5ffb807ca1
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-10-06 22:16:01 +0200

    Initial commit

---
+++ line_number_remover.py
@@ -0,0 +1,45 @@
+import string
+import os
+
+import keyboard
+import pyperclip
+from datetime import datetime
+
+
+def process_text(text):
+    lines = text.split('\n')
+    text = ""
+    for line in lines:
+        text += line.lstrip(string.digits).strip(' ') + '\n'
+    text.rstrip('\n')
+    return text
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def show_console_info():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    cls()
+    print("Last used: ", current_time)
+
+
+def callback():
+    text = pyperclip.paste()
+    text = process_text(text)
+    pyperclip.copy(text)
+    show_console_info()
+
+
+def main():
+    keyboard.add_hotkey("ctrl+shift", callback)
+    try:
+        keyboard.wait()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
